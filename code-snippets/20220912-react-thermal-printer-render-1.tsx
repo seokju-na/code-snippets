35ffd0309d5d1ca98d23a5710d6ad05875d0a57b
Seokju Na <seokju.me@gmail.com>
2022-09-12 21:52:20 +0900

    update

---
+++ code-snippets/20220912-react-thermal-printer-render-1.tsx
@@ -0,0 +1,3 @@
+import { ReactElement } from 'react';
+
+declare function render(elem: ReactElement): Promise<Uint8Array>;
---
+++ code-snippets/20220912-react-thermal-printer-render-2.tsx
@@ -0,0 +1,9 @@
+const data = await render(
+  <Printer characterSet="euc-kr">
+    <Text>왼쪽 정렬 텍스트</Text>
+    <Text align="center">가운데 정렬 텍스트</Text>
+    <Image src="/image.png" />
+    <Line />
+  </Printer>
+);
+await writer.write(data);
