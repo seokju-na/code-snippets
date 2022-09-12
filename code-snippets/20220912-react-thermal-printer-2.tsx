3c5e7796760f01be25c40e8e112fc22b7bc15606
Seokju Na <seokju.me@gmail.com>
2022-09-12 21:30:35 +0900

    image.png

--- code-snippets/20220912-react-thermal-printer-2.tsx
+++ code-snippets/20220912-react-thermal-printer-2.tsx
@@ -1,6 +1,6 @@
 <Printer characterSet="euc-kr">
   <Text>왼쪽 정렬 텍스트</Text>
   <Text align="center">가운데 정렬 텍스트</Text>
-  <Image src="https://my-cdn.com/my-image" />
+  <Image src="/image.png" />
   <Line />
 </Printer>
